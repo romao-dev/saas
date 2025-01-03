cdef1fcee183fa077d92199c87c5cbf771d956bc
romao-dev <gustavo.romao.3345@gmail.com>
2025-01-03 17:33:03 -0300

    Remove venv directory from version control

---
+++ src/saas/saas/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from visits.models import PageVisit
+
+
+def home_page_view(request):
+
+    queryset = PageVisit.objects.all()
+
+    my_title = 'my page'
+    content = 'dem titties'
+    title_test = 'hi'
+
+    PageVisit.objects.create()
+
+    my_context = {
+        'page_title': my_title,
+        'page_content': content,
+        'title_test' : title_test,
+        'queryset' : queryset,
+        'page_count' : queryset.count(),
+    } 
+    
+    return render(request, 'home.html', my_context)
